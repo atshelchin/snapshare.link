1eb1ba98b022c0676037f9c14bc33451b59ea265
Shelchin <hi@shelchin.com>
2025-10-23 13:10:26 +0800

    Add debug code

--- src/routes/api/create-presigned-upload-url/+server.ts
+++ src/routes/api/create-presigned-upload-url/+server.ts
@@ -4,8 +4,10 @@ import { createPresignedUploadUrl } from '$lib';
 
 export const POST: RequestHandler = async ({ request, platform }) => {
     try {
-        const { fileSizeBytes } = await request.json();
         console.log({ platform })
+        const { fileSizeBytes } = await request.json();
+
+        console.log({ fileSizeBytes })
         const result = await createPresignedUploadUrl(platform?.env, fileSizeBytes);
         return Response.json({ success: true, data: result });
     } catch (error) {
